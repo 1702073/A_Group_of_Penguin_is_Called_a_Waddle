971723e331e0f1953952e7a878d7e9cc210415cf
MUKILTEO\1702073 <1702073@mukilteo.wednet.edu>
2025-12-15 11:38:22 -0800

    u

--- Assets/Dexton/Scripts/Item Scripts/Drops.cs
+++ Assets/Dexton/Scripts/Item Scripts/Drops.cs
@@ -1,23 +1,23 @@
-//using UnityEngine;
+using UnityEngine;
 
-//public class Drops : MonoBehaviour
-//{
+public class Drops : MonoBehaviour
+{
 
-//    OnTriggerEnter2D(Collider2D collision)
-//    {
-//        if (collision.CompareTag("Player"))
-//        {
-//            switch (DropType)
-//            {
-//                case DropType.Health:
-//                collision.gameObject.GetComponent<Player_Health>()?.Heal(1);
-//                    break;
-//            }
-//        }
-//    }
-//    public enum DropType
-//    {
-//        Health = 0,
-//        Buff = 1
-//    }
-//}
+    OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            switch (DropType)
+            {
+                case DropType.Health:
+                    collision.gameObject.GetComponent<Player_Health>()?.Heal(1);
+                    break;
+            }
+        }
+    }
+    public enum DropType
+    {
+        Health = 0,
+        Buff = 1
+    }
+}
