29bddf26712c530d1ac5b4b5946813e61ea03e5d
fgreenmun@gmail.com <fgreenmun@gmail.com>
2025-12-16 18:32:15 -0800

    asef

--- Assets/Finn/Bez/PolarityObject.cs
+++ Assets/Finn/Bez/PolarityObject.cs
@@ -1,20 +1,27 @@
 using UnityEngine;
 using UnityEngine.UI;
+
 public class PolarityObject : MonoBehaviour
 {
     SpriteRenderer sr;
     Image img;
+    bool lastPolarity;
 
     void Start()
     {
         img = GetComponent<Image>();
         sr = GetComponent<SpriteRenderer>();
+        if (PolarityManager.Instance != null)
+        {
+            lastPolarity = PolarityManager.Instance.IsWhitePolarity;
+        }
         UpdateColor();
     }
    
     void Update()
     {
         UpdateColor();
+        CheckPolarityChange();
     }
     
     void UpdateColor()
@@ -23,7 +30,26 @@ public class PolarityObject : MonoBehaviour
         {
             Color targetColor = PolarityManager.Instance.GetCurrentColor();
             sr.material.SetColor("_Color", targetColor);
-            img.material.SetColor("_Color", targetColor);
+            if (img != null) img.material.SetColor("_Color", targetColor);
+        }
+    }
+    
+    void CheckPolarityChange()
+    {
+        if (PolarityManager.Instance != null && PolarityManager.Instance.IsWhitePolarity != lastPolarity)
+        {
+            lastPolarity = PolarityManager.Instance.IsWhitePolarity;
+            SpawnVFX();
+        }
+    }
+    
+    void SpawnVFX()
+    {
+        if (PolarityManager.Instance != null && PolarityManager.Instance.polaritySwitchVFX != null)
+        {
+            Vector3 spawnPos = transform.position;
+            GameObject vfx = Instantiate(PolarityManager.Instance.polaritySwitchVFX, spawnPos, Quaternion.identity);
+            Destroy(vfx, 2f);
         }
     }
 }
